ec8bf1f4d39fd7ee5957aa4d8181396a1a8bc039
ggelmi <ggredelmi@gmail.com>
2020-12-14 19:50:47 -0500

    test main modification

--- kalmanFilter/motion_models/test_main.cpp
+++ kalmanFilter/motion_models/test_main.cpp
@@ -30,22 +30,27 @@ std::cout << "The predicted state : \n " << predState << std::endl;
 // testing the coordinated turn model
 
 const double Delta = 1;
-const double sigmaPhi = 0.25;
-const double sigmaOmega = 0.5;
+const double sigmaV = 0.01;
+const double sigmaOmega = 0.2;
 unsigned int dimen= 5;
 
-CoordinatedTurnModel  ct_model(Delta,sigmaPhi,sigmaOmega,dimen);
+CoordinatedTurnModel  ct_model(Delta,sigmaV,sigmaOmega,dimen);
 
 std::cout << "The dimension is : " << ct_model.getDimension() << std::endl;
 
 Eigen::VectorXd currState (dimen);
 
-currState << 1,1,0,2.7,0;
+currState << 1,1,0.3,2.7,0;
+
+std::cout << "currState is " << currState << std::endl;
 
 Eigen::VectorXd predState =  ct_model.predictState(currState);
 
 std::cout << "The predicted state : \n " << predState << std::endl;
 
+std::cout << "The processNoise : \n" << ct_model.getProcessNoiseMatrix() << std::endl;
+
+std::cout << "The jacobian : \n" << ct_model.getJacobianMatrix(currState) << std::endl;
 
 return 0;
 }
