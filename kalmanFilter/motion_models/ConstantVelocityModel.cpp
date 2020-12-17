f5aa8d5f256c05fd6c823561545285e23ea7a181
ggelmi <ggredelmi@gmail.com>
2020-12-17 13:35:47 -0500

    Adding commenting, indentation, and formatting

--- kalmanFilter/motion_models/ConstantVelocityModel.cpp
+++ kalmanFilter/motion_models/ConstantVelocityModel.cpp
@@ -1,64 +1,65 @@
+/* File ConstantVelocityMotionModel.cpp
+*//**
+*     @file ConstantVelocityMotionModel.cpp
+*     @brief This file implements the constant velocity motion model
+*     
+*     @author Guled Elmi, ggredelmi@gmail.com
+*     Created 12/14/2020
+*
+*/
+
 #include<cmath>
-
-
 #include"ConstantVelocityModel.h"
 
+namespace stateEstimation
+{
 
-
-
-ConstantVelocityModel::ConstantVelocityModel(const double& dtime, 
-		                             const double& sig, unsigned int& dimension)
- {
-
-	dt = dtime;
-	sigma = sig;
-	DIM = dimension;
- }
+ConstantVelocityModel::ConstantVelocityModel(const double& dtime,const double& sig, unsigned int& dimension)
+   { 
+	   dt = dtime;
+	   sigma = sig;
+	   DIM = dimension;
+   }
  
 unsigned int ConstantVelocityModel:: getDimension() const
- {
-
-     return DIM;
- }
-
-Eigen::VectorXd ConstantVelocityModel:: predictState( const Eigen::VectorXd& currState)
    {
-
-     
-     Eigen::VectorXd predicted_state =  getStateTransitionMatrix(currState)*currState;
-
-
-     return predicted_state; 
-
+      return DIM;
    }
 
+Eigen::VectorXd ConstantVelocityModel:: predictState( const Eigen::VectorXd& currState)
+   {  
+      Eigen::VectorXd predicted_state =  getStateTransitionMatrix(currState)*currState;
+     
+      return predicted_state; 
+   }
 
 Eigen::MatrixXd ConstantVelocityModel:: getStateTransitionMatrix(const Eigen::VectorXd& currState) 
    {
 	
-	Eigen::MatrixXd stateTransitionMatrix (getDimension(),getDimension());
+	   Eigen::MatrixXd stateTransitionMatrix (getDimension(),getDimension());
 
-	stateTransitionMatrix << 1,0,dt,0,
-       		                 0,1,0,dt,
-                                 0,0,1,0,
-			         0,0,0,0;
+	   stateTransitionMatrix << 1,0,dt,0,
+       		                   0,1,0,dt,
+                               0,0,1,0,
+			                      0,0,0,0;
 
-       return stateTransitionMatrix;	
-	                  
+       return stateTransitionMatrix;	                  
    }
 
 Eigen::MatrixXd ConstantVelocityModel::  getProcessNoiseCovariance() 
    {
-       Eigen::MatrixXd processNoiseMatrix(getDimension(),getDimension());
+      Eigen::MatrixXd processNoiseMatrix(getDimension(),getDimension());
 
-       processNoiseMatrix << std::pow(dt, 4)/4,  0, std::pow(dt, 3)/2,0,
-                       0, std::pow(dt, 4)/4, 0, std::pow(dt, 3)/2,
-                       std::pow(dt, 3)/2, 0,  std::pow(dt, 2), 0,
-                       0,std::pow(dt, 3)/2, 0,    std::pow(dt, 2);
+      processNoiseMatrix << std::pow(dt, 4)/4,  0, std::pow(dt, 3)/2,0,
+                            0, std::pow(dt, 4)/4, 0, std::pow(dt, 3)/2,
+                            std::pow(dt, 3)/2, 0,  std::pow(dt, 2), 0,
+                            0,std::pow(dt, 3)/2, 0,    std::pow(dt, 2);
        
-       processNoiseMatrix = std::pow(sigma,2) * processNoiseMatrix;
-       return processNoiseMatrix;
-   }
+      processNoiseMatrix = std::pow(sigma,2) * processNoiseMatrix;
 
+      return processNoiseMatrix;
+   }
+   
+}
 
 
