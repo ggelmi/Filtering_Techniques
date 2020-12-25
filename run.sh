584f092e12148b84f101e58f6b817da1351e1828
ggelmi <ggredelmi@gmail.com>
2020-12-24 22:27:34 -0500

    Modifying the script to accomodate the EKF test run

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-octave simulate 
+: <<'END'
+pushd matlab_simulator/linear_simulator
+octave kf_simulate 
+popd
 
 pushd /home/guuto/octave/Filtering_Techniques/mainApp/build
 
@@ -10,5 +12,20 @@ make
 
 popd
 
-octave --persist visualize
+pushd matlab_simulator/linear_simulator
+octave --persist kf_visualize
+popd
+END
+pushd matlab_simulator/non_linear_simulator
+octave ekf_simulate.m
+popd
+pushd /home/guuto/octave/Filtering_Techniques/mainApp/build
 
+cmake ..
+make
+./almis_app_exe
+
+popd
+pushd matlab_simulator/non_linear_simulator
+octave --persist ekf_visualize.m
+popd
